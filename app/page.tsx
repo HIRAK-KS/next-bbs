a9f7a52be06e4e7d717d7bc5483e189b47553943
admin <hirak.keisuke@gmail.com>
2024-02-29 20:27:42 +0900

    Add files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import BBSCardList from "./components/BBSCardList";
+import { BBSData } from "./types/types";
 
-export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+async function getBBSAllData() {
+  const response = await fetch("http://localhost:3000/api/post", {
+    cache: "no-store",
+  });
+
+  const bbsAllData: BBSData[] = await response.json();
+
+  return bbsAllData;
+}
+
+export default async function Home() {
+  const bbsAllData = await getBBSAllData();
+
+  return (
+    <main>
+      <BBSCardList bbsAllData={bbsAllData} />
+    </main>
+  );
 }
